451efb144843d70dd7b7a446756184baf646f569
seunghwan <sun1238799@naver.com>
2023-11-03 22:32:41 +0900

    chore: globalStyle 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
+import '../styles/globalStyle.scss';
 
 const notosanskr = Noto_Sans_KR({
   weight: '400',
