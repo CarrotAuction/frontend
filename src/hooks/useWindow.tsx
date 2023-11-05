35a3e939074b527fe9ebfd68d45914b3587ecf0c
seunghwan <sun1238799@naver.com>
2023-11-05 12:01:18 +0900

    refactor: useWindowSize hook 수정

--- src/hooks/useWindow.tsx
+++ src/hooks/useWindow.tsx
@@ -1,31 +1,36 @@
-import { useState } from 'react';
 import { throttle } from 'lodash';
+import React, { useState } from 'react';
 
 type WindowSize = {
   width: number;
   height: number;
 };
 
-type ReturnType = [windowSize: WindowSize, eventListener: () => () => void];
+type ReturnType = [
+  windowSize: WindowSize,
+  windowEventListener: () => () => void,
+];
 
 const useWindowSize = (): ReturnType => {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
-  const resizeViewPort = throttle(() => {
+
+  const changeViewPort = throttle(() => {
     setWindowSize({
       width: window.innerWidth,
       height: window.innerHeight,
     });
-  }, 300);
-  const eventListener = () => {
-    window.addEventListener('resize', resizeViewPort);
+  }, 500);
+
+  const windowEventListener = () => {
+    window.addEventListener('resize', changeViewPort);
     return () => {
-      window.removeEventListener('resize', resizeViewPort);
+      window.removeEventListener('resize', changeViewPort);
     };
   };
-  return [windowSize, eventListener];
+  return [windowSize, windowEventListener];
 };
 
 export default useWindowSize;
