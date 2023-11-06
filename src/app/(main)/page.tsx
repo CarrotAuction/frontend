4f838c241af67488eb860299888b4ce9877199ee
seunghwan <sun1238799@naver.com>
2023-11-06 17:13:48 +0900

    feat: 메인페이지 배너 컴포넌트 퍼블리싱

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import MainIntroduce from '@/src/components/MainIntroduce';
+import Banner from '@/src/components/Banner';
 import styles from './index.module.scss';
 
 export default function Home() {
   return (
     <main>
-      <div className={styles.dummy}>d</div>
+      <Banner />
       <MainIntroduce />
     </main>
   );
--- src/components/MainIntroduce/index.tsx
+++ src/components/MainIntroduce/index.tsx
@@ -54,23 +54,23 @@ const MainIntroduce = () => {
           </div>
         </article>
         <article className={styles.introText}>
-          <p>Carrot Auction이 무엇인가요?</p>
-          <p>도움이 필요한 당신, 지금 나에게 필요한 것은?</p>
+          <p>무엇을 할 수 있나요?</p>
+          <p>우리 동네 사람과 소통해요</p>
           <span>
-            이어드림에서 당신에게 필요한 모든것을 해결해보세요 간편하게
+            Carrot Auction에서는 같은 지역 사람들과 원하는 물건들을
             <br />
-            상담예약하고 채팅과 전화로 무엇이든 물어볼수있어요
+            경매 형식으로 사고 팔 수 있어요
           </span>
         </article>
       </section>
       <section className={styles.introBox}>
         <article className={styles.introText}>
-          <p>Carrot Auction이 무엇인가요?</p>
-          <p>도움이 필요한 당신, 지금 나에게 필요한 것은?</p>
+          <p>어떤 점이 좋나요?</p>
+          <p>매물을 늦게 발견해도 기회가 있어요!</p>
           <span>
-            이어드림에서 당신에게 필요한 모든것을 해결해보세요 간편하게
+            경매 참여자들이 제시한 금액을 확인할 수 있기 때문에
             <br />
-            상담예약하고 채팅과 전화로 무엇이든 물어볼수있어요
+            매물을 늦게 발견하더라도 더 높은 금액을 제시하여 구매할 수 있어요
           </span>
         </article>
         <article className={styles.introImg}>
