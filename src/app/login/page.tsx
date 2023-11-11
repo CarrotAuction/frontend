f06429a288a6c9e1a257b23a1377342eb478be57
seunghwan <sun1238799@naver.com>
2023-11-11 16:58:39 +0900

    design: 로그인 페이지 퍼블리싱

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,63 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
+import { GiCancel } from 'react-icons/gi';
+import Link from 'next/link';
+import useInput from '@/src/hooks/useInput';
 import styles from './index.module.scss';
 
 const Login = () => {
+  const [id, changeId, resetId] = useInput();
+  const [password, changePassword] = useInput();
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const togglePassword = () => {
+    setIsVisible((pre) => !pre);
+  };
+
+  const sendLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (id === '' || password === '') {
+      alert('아이디 또는 비밀번호를 입력해주세요 ');
+    }
+  };
+
   return (
     <main className={styles.loginPage}>
-      <h1>로그인</h1>
-      <form>
+      <h1 className={styles.title}>로그인</h1>
+      <form className={styles.form}>
         <label htmlFor="id">아이디</label>
-        <input type="text" id="id" />
+        <div className={styles.inputBox}>
+          <input onChange={changeId} value={id} type="text" id="id" />
+          {id && <GiCancel onClick={resetId} className={styles.icon} />}
+        </div>
         <label htmlFor="password">비밀번호</label>
-        <input type="password" id="password" />
-        <button type="button">Submit</button>
+        <div className={styles.inputBox}>
+          <input
+            onChange={changePassword}
+            value={password}
+            type={isVisible ? 'text' : 'password'}
+            id="password"
+          />
+          {password &&
+            (isVisible === true ? (
+              <AiFillEye onClick={togglePassword} className={styles.icon} />
+            ) : (
+              <AiFillEyeInvisible
+                onClick={togglePassword}
+                className={styles.icon}
+              />
+            ))}
+        </div>
+        <button className={styles.submit} onClick={sendLogin} type="button">
+          로그인
+        </button>
       </form>
-      <p>회원가입하러 가기</p>
+      <p className={styles.signUp}>
+        <Link href="/signUp">회원가입하러가기</Link>
+      </p>
     </main>
   );
 };
