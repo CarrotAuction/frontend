bc80d6dfcb76a4e92a4cb23d5fc8493037cde195
Kimyebin00 <kimyeobin12@gmail.com>
2023-11-23 23:16:37 +0900

    chore: 이미지 도메인 next.congif.js에 설정 추가

--- src/hooks/usePreview.tsx
+++ src/hooks/usePreview.tsx
@@ -9,17 +9,18 @@ const UsePreview = () => {
     const target = e.target as HTMLInputElement;
 
     if (!target.files) return;
-
     const selectedFile = target.files[0];
     setFile(selectedFile);
     const reader = new FileReader();
-    reader.readAsDataURL(selectedFile);
-    reader.onloadend = () => {
-      if (reader.readyState === 2) {
-        // 파일 onLoad가 성공하면 2, 진행 중은 1, 실패는 0 반환
-        setImage(reader.result as string);
-      }
-    };
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        if (reader.readyState === 2) {
+          // 파일 onLoad가 성공하면 2, 진행 중은 1, 실패는 0 반환
+          setImage(reader.result as string);
+        }
+      };
+    }
   };
 
   return { file, image, handleImage };
