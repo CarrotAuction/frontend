be9fe8822d60b2912b2ba1dd9683b61a1d2f4f78
seunghwan <sun1238799@naver.com>
2023-11-24 11:16:20 +0900

    feat: 스토리북 nav바 ui 테스팅

--- src/common/Nav/index.stories.tsx
+++ src/common/Nav/index.stories.tsx
@@ -2,26 +2,34 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import NavBar from '.';
 
-// 내 스토리가 기본적으로 어디에 있는지 알려줌
 const meta: Meta<typeof NavBar> = {
-  title: 'NavBar',
+  title: 'UI/atom/NavBar',
   component: NavBar,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '3em' }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof NavBar>;
 
-export const Navbar: Story = {
+export const Default: Story = {
   parameters: {
     nextjs: {
       appDirectory: true,
     },
+    backgrounds: {
+      values: [
+        { name: 'red', value: '#f00' },
+        { name: 'green', value: '#0f0' },
+        { name: 'blue', value: '#00f' },
+      ],
+    },
   },
-  // 여기에 필요한 Arg는 구성 요소에 따라 다릅니다
-  args: {
-    // primary: true,
-    // label: 'Button',
-  },
-  // render: () => <NavBar />,
+  args: {},
 };
